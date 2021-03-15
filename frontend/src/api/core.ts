d8834c372c8f4c49a7a38c048325ffc3565ad0ef
barak <barak.drechsler@gmail.com>
2021-03-15 13:38:18 +0200

    follow create reacp app convention

--- frontend/src/api/core.ts
+++ frontend/src/api/core.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ApiError } from "../utils/errorTypes";
 
-const API_BASE_URL = process.env.REACT_API_BASE_URL || '';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
 export const getAPIPath = (path: string) => `${API_BASE_URL}/api/${path}`;
 
 export async function request<TData>(
